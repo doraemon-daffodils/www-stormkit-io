60b54d08b97cd39cbbc24404692fb7a58a16207f
doraemon-daffodils <81766259+doraemon-daffodils@users.noreply.github.com>
2022-05-01 18:43:41 +0530

    Update GdprBanner.vue

--- components/molecules/GdprBanner.vue
+++ components/molecules/GdprBanner.vue
@@ -12,10 +12,14 @@
         </div>
         <h2 class="font-bold text-2xl">Disclaimer</h2>
       </header>
-      <main class="font-light max-w-2xl text-center mb-8">
-<p> This website has been designed only for the purposes of dissemination of basic information on _____; information which is otherwise available on the internet, various public platforms and social media. Careful attention has been given to ensure that the information provided herein is accurate and up-to-date. However, we are not responsible for any reliance that a reader places on such information and shall not be liable for any loss or damage caused due to any inaccuracy in or exclusion of any information, or its interpretation thereof. Reader is advised to confirm the veracity of the same from independent and expert sources.</p>
+      <main class="font-light text-justify  max-w-2xl text-center mb-8">
+<p> This website has been designed only for the purposes of dissemination of basic information on _____; information which is otherwise available on the internet, various public platforms and social media.</p>
 
-<p> This website is not an attempt to advertise or solicit clients, and does not seek to create or invite any lawyer-client relationship. The links provided on this website are to facilitate access to basic information on ____, and to share the various thought leadership initiatives undertaken by it. The content herein or on such links should not be construed as a legal reference or legal advice. Readers are advised not to act on any information contained herein or on the links and should refer to legal counsels and experts in their respective jurisdictions for further information and to determine its impact.</p>
+<p>Careful attention has been given to ensure that the information provided herein is accurate and up-to-date. However, we are not responsible for any reliance that a reader places on such information and shall not be liable for any loss or damage caused due to any inaccuracy in or exclusion of any information, or its interpretation thereof. Reader is advised to confirm the veracity of the same from independent and expert sources.</p>
+
+<p> This website is not an attempt to advertise or solicit clients, and does not seek to create or invite any lawyer-client relationship.</p>
+
+<p> The links provided on this website are to facilitate access to basic information on ____, and to share the various thought leadership initiatives undertaken by it. The content herein or on such links should not be construed as a legal reference or legal advice. Readers are advised not to act on any information contained herein or on the links and should refer to legal counsels and experts in their respective jurisdictions for further information and to determine its impact.</p>
 
 <p> We advise against the use of the communication platform provided on this website for exchange of any confidential, business or politically sensitive information. User is requested to use his or her judgment and exchange of any such information shall be solely at the user’s risk.</p>
 
